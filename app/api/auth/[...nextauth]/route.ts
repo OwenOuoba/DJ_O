b91e7daa3b80c5a8b16f2848d1746eda7944facc
OwenOuoba <touob053@uottawa.ca>
2026-05-11 23:28:18 -0400

    feat: initial setup with Spotify OAuth and NextAuth

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,31 @@
+import NextAuth from "next-auth";
+import SpotifyProvider from "next-auth/providers/spotify";
+
+export const authOptions ={
+  providers: [
+    SpotifyProvider({
+      clientId: process.env.SPOTIFY_CLIENT_ID!,
+      clientSecret: process.env.SPOTIFY_CLIENT_SECRET!,
+      authorization: {
+        params: {
+          scope: "user-top-read user-read-recently-played playlist-modify-public",
+       },
+     },
+}),
+  ],
+ callbacks:{
+    async jwt({ token, account }) {
+        if (account) {
+            token.accessToken = account.access_token
+        }
+        return token
+    },
+    async session({ session, token, user }) {
+        session.accessToken = token.accessToken
+        return session
+    }
+ }
+};
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
